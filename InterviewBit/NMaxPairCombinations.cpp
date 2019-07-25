9a899982258ece7e093a575101dc50a9ffa724ec
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-07-25 17:51:01 +0530

    imp

--- InterviewBit/NMaxPairCombinations.cpp
+++ InterviewBit/NMaxPairCombinations.cpp
@@ -1,35 +1,27 @@
 vector<int> Solution::solve(vector<int> &A, vector<int> &B) {
+    int n = A.size();
+    priority_queue<pair<int, pair<int, int>>> pq;
+    set<pair<int, int>> indices;
     sort(A.begin(), A.end());
     sort(B.begin(), B.end());
-    int n = A.size();
-    set<pair<int, int>> indices;
-    priority_queue<pair<int, pair<int, int>>> pq;
-    pq.push({ A[n-1] + B[n-1], { n-1, n-1 } });
-    indices.insert({ n-1, n-1 });
-    int k = A.size();
-    vector<int> res;
-    while(k--) {
-        pair<int, pair<int, int>> temp = pq.top();
+    vector<int> ret;
+    pq.push({ A[n - 1] + B[n - 1], { n - 1, n - 1 }});
+    indices.insert({ n - 1, n - 1 });
+    while(n--) {
+        auto t = pq.top();
         pq.pop();
-        res.push_back(temp.first);
-        
-        int i = temp.second.first;
-        int j = temp.second.second;
-        
-        int sum = A[i-1] + B[j];
-        pair<int, int> p1 = {i-1, j};
-        if(indices.find(p1) == indices.end()) {
-            indices.insert(p1);
-            pq.push({ sum, p1 });
+        ret.push_back(t.first);
+        int x = t.second.first - 1, y = t.second.second;
+        if(indices.find({ x, y }) == indices.end()) {
+            indices.insert({ x, y });
+            pq.push({ A[x] + B[y], { x, y } });
         }
-        
-        sum = A[i] + B[j-1];
-        p1 = {i, j-1};
-        if(indices.find(p1) == indices.end()) {
-            indices.insert(p1);
-            pq.push({ sum, p1 });
+        x = t.second.first, y = t.second.second - 1;
+        if(indices.find({ x, y }) == indices.end()) {
+            indices.insert({ x, y });
+            pq.push({ A[x] + B[y], { x, y } });
         }
     }
-    return res;
+    return ret;
 }
 
