61e3be77ba0cd4c6587bb5a0452e446df90eba76
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-07-25 14:42:37 +0530

    easy

--- InterviewBit/MergeKSortedLinkedLists.cpp
+++ InterviewBit/MergeKSortedLinkedLists.cpp
@@ -6,11 +6,11 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-
-struct cmp {
+class cmp {
+    public:
     bool operator()(ListNode* A, ListNode* B) {
-        return (A -> val > B -> val);
-    } 
+        return A -> val > B -> val;
+  }  
 };
  
 ListNode* Solution::mergeKLists(vector<ListNode*> &A) {
@@ -19,21 +19,18 @@ ListNode* Solution::mergeKLists(vector<ListNode*> &A) {
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
     priority_queue<ListNode*, vector<ListNode*>, cmp> pq;
-    for(int i=0; i<A.size(); i++) {
+    for(int i = 0;  i < A.size(); i++) {
         pq.push(A[i]);
     }
-    
-    ListNode* res = new ListNode(0);
-    ListNode* head = res;
+    ListNode *ret = new ListNode(0), *curr = ret;
     while(!pq.empty()) {
-        ListNode* top = pq.top();
+        ListNode* temp = pq.top();
         pq.pop();
-        res -> next = top;
-        res = res -> next;
-        
-        if(top -> next)
-            pq.push(top -> next);
+        curr -> next = temp;
+        curr = curr -> next;
+        if(temp -> next)
+            pq.push(temp -> next);
     }
-    return head -> next;
+    return ret -> next;
 }
 
